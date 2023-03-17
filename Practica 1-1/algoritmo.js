193df99ee4492ff2e7372577f37b6d2abc79f490
Roberto Alagemovits <robertomovits@gmail.com>
2023-03-17 10:16:29 -0300

    primer commit

---
+++ Practica 1-1/algoritmo.js
@@ -0,0 +1,7 @@
+document.querySelector("body").innerHTML=`
+<h1>yooooooooooo</h1>
+<H2>yooooooooo</H2>
+<h3>yooooooo</h3>
+<img src="https://i.ytimg.com/vi/VKMw2it8dQY/sddefault.jpg" alt="">
+<img src="https://i.ytimg.com/vi/VKMw2it8dQY/sddefault.jpg" alt="">
+`
