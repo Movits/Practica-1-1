ad2f471c438fff7ab42f686cc3772cd72c5c4f6d
Roberto Alagemovits <robertomovits@gmail.com>
2023-03-17 12:34:01 -0300

    cambie los nombres de las imagenes y el texto

--- Practica 1-1/algoritmo.js
+++ Practica 1-1/algoritmo.js
@@ -1,7 +1,7 @@
 document.querySelector("body").innerHTML=`
-<h1>yooooooooooo</h1>
-<H2>yooooooooo</H2>
-<h3>yooooooo</h3>
-<img src="https://i.ytimg.com/vi/VKMw2it8dQY/sddefault.jpg" alt="">
-<img src="https://i.ytimg.com/vi/VKMw2it8dQY/sddefault.jpg" alt="">
+<h1>Practica 1-1</h1>
+    <H2>Frontend</H2>
+    <h3>Backend</h3>
+    <img src="https://i.ytimg.com/vi/VKMw2it8dQY/sddefault.jpg" alt="hombre">
+    <img src="Imagenes/yoooooo.jpg" alt="hombre 2">
 `
